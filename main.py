c2daf01d05c0c8b7b81e6678c633e89d6882d404
cheongy <cheongy@eudc.college-montreal.qc.ca>
2024-02-01 14:44:39 -0500

    vgujhk,

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import arcade
 import random
 
+#longueur et largeur de l'écran
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+#liste de couleur
 COLORS = [arcade.csscolor.BEIGE, arcade.csscolor.RED, arcade.csscolor.TAN, arcade.csscolor.BLUE, arcade.csscolor.CORAL, arcade.csscolor.DARK_OLIVE_GREEN, arcade.csscolor.GOLD, arcade.csscolor.GREEN]
 
+#rectangle
 class rectangle:
+    #caractéristiques de rectangle
     def __init__(self, x, y):
         self.longueur = 21
         self.largeur = 10
@@ -17,6 +21,7 @@ class rectangle:
         self.tilt_angle = 90
         self.colors = random.choice(COLORS)
 
+    #mouvement du rectangle
     def on_update(self):
         self.centre_x += self.change_x
         self.centre_y += self.change_y
@@ -35,13 +40,16 @@ class rectangle:
     def setup(self):
         pass
 
+    #fonction pour dessiner un rectangle
     def on_draw(self):
         arcade.draw_rectangle_filled(self.centre_x, self.centre_y, self.longueur, self.largeur, self.colors, self.tilt_angle)
 
 
 
-
+#cercle
 class cercle:
+
+    #caractéristiques du cercle
     def __init__(self, x, y):
         self.rayon = 15
         self.change_x = 7
@@ -50,6 +58,7 @@ class cercle:
         self.centre_y = y
         self.colors = random.choice(COLORS)
 
+    #mouvement du cercle
     def on_update(self):
         self.centre_x += self.change_x
         self.centre_y += self.change_y
@@ -67,16 +76,21 @@ class cercle:
 
     def setup(self):
         pass
+
+    #dessiner un cercle
     def on_draw(self):
         arcade.draw_circle_filled(self.centre_x, self.centre_y, self.rayon, self.colors)
 
 
 class MyGame(arcade.Window):
+
+    #faire une liste de formes
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
         self.list_rectangle = []
         self.list_cercle = []
 
+    #chercher un cercle ou un rectangle au hasard dans la liste
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         if button == arcade.MOUSE_BUTTON_LEFT:
             self.list_cercle.append(cercle(x,y))
@@ -84,6 +98,8 @@ class MyGame(arcade.Window):
             self.list_rectangle.append(rectangle(x, y))
     def setup(self):
         pass
+
+    #faire bouger le cercle ou rectangle
     def on_update(self, delta_time: float):
         for cercle in self.list_cercle:
             cercle.on_update()
@@ -91,6 +107,7 @@ class MyGame(arcade.Window):
         for rectangle in self.list_rectangle:
             rectangle.on_update()
 
+    #dessiner la forme prise de la liste
     def on_draw(self):
         arcade.start_render()
         for cercle in self.list_cercle:
@@ -100,7 +117,7 @@ class MyGame(arcade.Window):
             rectangle.on_draw()
 
 
-
+#commencer le jeu
 def main():
     my_game = MyGame()
     my_game.setup()
